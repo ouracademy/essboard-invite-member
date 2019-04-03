33cf717dd8977a2545d5a88aaa34f7a4f37f5854
artmadeit <this.arthur.email@gmail.com>
2019-04-03 15:57:48 -0500

    feat: add dynamic port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const { sendMail } = require('./send-mail')
 
+app.set("port", port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
