2cc8b47a7292a9be330a208c0212e9103df969ab
qpdian <qpdiam@gmail.com>
2019-04-03 19:12:11 -0500

    feat: add read variables from environment

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express()
-const port = process.env.PORT || 3000
+require("./env");
+const express = require("express");
+var bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
 
-const { sendMail } = require('./send-mail')
+const { sendMail } = require("./send-mail");
 
 app.set("port", port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', async (req, res) => {
-    const { from, to, project } = req.body
+app.post("/", async (req, res) => {
+    const { from, to, project } = req.body;
 
     const info = await sendMail(from, to, project);
-    return res.send(info)
-})
+    return res.send(info);
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
