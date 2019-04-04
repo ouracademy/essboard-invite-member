d202a569bf7fd479e38f2677cf7a278a66699982
qpdian <qpdiam@gmail.com>
2019-04-03 19:41:33 -0500

    feat: add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("./env");
 const express = require("express");
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,6 +9,7 @@ const { sendMail } = require("./send-mail");
 
 app.set("port", port);
 app.use(bodyParser.json());
+app.use(cors({ origin: process.env.ORIGIN }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/", async (req, res) => {
